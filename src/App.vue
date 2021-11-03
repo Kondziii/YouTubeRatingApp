8aa32f8007ce8dc551325af9f4e07d995c7de9f8
Kondziii <1999konrad@gmail.com>
2021-11-03 17:52:00 +0100

    backgorund added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,47 @@
 <template>
-  <div></div>
+  <div class="root"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ytApi from './data/yt-api';
+import Channel_url from './enums/Channel_url';
+import { useStore } from './store';
+import { useChannelActions } from './store/channel/actions';
 
 export default defineComponent({
   setup() {
+    const store = useStore();
+    const ChannelActions = useChannelActions();
+
     return {};
   },
 });
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.root {
+  width: 100%;
+  height: 100vh;
+  background-image: url('./assets/background_1.jpg');
+  background-position: center;
+  background-size: cover;
+  position: relative;
+}
+
+.root::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(100, 100, 100, 0.3);
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+</style>
