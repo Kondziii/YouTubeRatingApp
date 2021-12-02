7626eaab1f5a792a8bc1bf530263ef9acac6b2db
Kondziii <1999konrad@gmail.com>
2021-12-02 12:48:23 +0100

    before migrating to server

--- server/app.ts
+++ server/app.ts
@@ -8,6 +8,16 @@ import { Request, Response, NextFunction } from 'express';
 const app = express();
 app.use(bodyParser.json());
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use('/channels', channelRoutes);
 app.use('/videos', videoRoutes);
 
