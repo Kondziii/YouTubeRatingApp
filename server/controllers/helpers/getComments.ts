8eec7c3aa7746eff3fce188edd70cd73b9ecfd6d
Kondziii <1999konrad@gmail.com>
2021-12-31 12:49:50 +0100

    getComments reformated

--- server/controllers/helpers/getComments.ts
+++ server/controllers/helpers/getComments.ts
@@ -9,6 +9,10 @@ import {
   BASIC_SENTIMENT_THRESHOLD,
 } from '../../config';
 import { CommentThread, CommentThreadItem, Comment } from '../../types/Comment';
+import { Sentiment } from '../../types/Sentiment';
+const vader = require('vader-sentiment');
+const { Language } = require('node-nlp');
+const LanguageDetector = new Language();
 
 export const getComments = async (
   videoId: string,
@@ -104,4 +108,97 @@ export const stateCompound = (
   } else vote = 'neutral';
 
   return vote;
+};
+
+export const getVideoSentimentHelper = async (
+  params: { videoId: string },
+  query: { channelId: string; subcomments: boolean }
+) => {
+  try {
+    let comments: Array<Comment>;
+
+    comments = await getComments(
+      params.videoId,
+      query.channelId,
+      query.subcomments
+    );
+
+    const commentCount = {
+      all: 0,
+      processed: 0,
+      excluded: 0,
+    };
+
+    const commentVoteCount = {
+      positive: 0,
+      neutral: 0,
+      negative: 0,
+    };
+
+    type avgOptions = {
+      [key: string]: number;
+    };
+
+    const avg: avgOptions = {
+      positive: 0,
+      neutral: 0,
+      negative: 0,
+      compound: 0,
+    };
+
+    for (const comment of comments) {
+      let language;
+      try {
+        language = LanguageDetector.guessBest(comment.text, [
+          'en',
+          'pl',
+        ]).language;
+      } catch (error) {
+        console.log(
+          `Language undetected. Comment ${comment.text} will be excluded.`
+        );
+      }
+
+      if (language && language === 'English') {
+        const sentiment: Sentiment =
+          await vader.SentimentIntensityAnalyzer.polarity_scores(comment.text);
+
+        if (sentiment.compound >= BASIC_SENTIMENT_THRESHOLD)
+          commentVoteCount.positive++;
+        else if (sentiment.compound <= -BASIC_SENTIMENT_THRESHOLD)
+          commentVoteCount.negative++;
+        else commentVoteCount.neutral++;
+
+        avg.positive += sentiment.pos;
+        avg.neutral += sentiment.neu;
+        avg.negative += sentiment.neg;
+        avg.compound += sentiment.compound;
+      } else {
+        commentCount.excluded++;
+      }
+      commentCount.all++;
+    }
+    commentCount.processed = commentCount.all - commentCount.excluded;
+
+    Object.keys(avg).forEach((key) => {
+      if (avg[key] !== 0) {
+        avg[key] /= commentCount.processed;
+      }
+    });
+
+    const vote =
+      commentCount.processed === 0
+        ? 'unknown'
+        : stateCompound(avg.compound, commentCount.processed);
+
+    return {
+      vote,
+      commentCount,
+      commentVoteCount,
+      avg,
+      date: new Date().toISOString(),
+    };
+  } catch (error) {
+    throw error;
+  }
 };
--- server/controllers/videos.ts
+++ server/controllers/videos.ts
@@ -1,14 +1,15 @@
 import { Comment } from './../types/Comment';
 import { Sentiment } from './../types/Sentiment';
-import { getComments, stateCompound } from './helpers/getComments';
+import {
+  getComments,
+  stateCompound,
+  getVideoSentimentHelper,
+} from './helpers/getComments';
 import { VideoBySearch, VideoById, Video } from './../types/Video';
 import axios from 'axios';
 import { KEY, URL_YOUTUBE_API } from '../config';
 import { RequestHandler } from 'express';
 import { BASIC_SENTIMENT_THRESHOLD } from '../config';
-const vader = require('vader-sentiment');
-const { Language } = require('node-nlp');
-const LanguageDetector = new Language();
 
 export const getVideosByKeyWord: RequestHandler = async (req, res, next) => {
   const params = req.params as { keyWord: string };
@@ -109,90 +110,11 @@ export const getVideoSentiment: RequestHandler = async (req, res, next) => {
     channelId: string;
     subcomments: boolean;
   };
-  let comments: Array<Comment>;
 
   try {
-    comments = await getComments(
-      params.videoId,
-      query.channelId,
-      query.subcomments
-    );
+    const result = await getVideoSentimentHelper(params, query);
 
-    const commentCount = {
-      all: 0,
-      processed: 0,
-      excluded: 0,
-    };
-
-    const commentVoteCount = {
-      positive: 0,
-      neutral: 0,
-      negative: 0,
-    };
-
-    type avgOptions = {
-      [key: string]: number;
-    };
-
-    const avg: avgOptions = {
-      positive: 0,
-      neutral: 0,
-      negative: 0,
-      compound: 0,
-    };
-
-    for (const comment of comments) {
-      let language;
-      try {
-        language = LanguageDetector.guessBest(comment.text, [
-          'en',
-          'pl',
-        ]).language;
-      } catch (error) {
-        console.log(
-          `Language undetected. Comment ${comment.text} will be excluded.`
-        );
-      }
-
-      if (language && language === 'English') {
-        const sentiment: Sentiment =
-          await vader.SentimentIntensityAnalyzer.polarity_scores(comment.text);
-
-        if (sentiment.compound >= BASIC_SENTIMENT_THRESHOLD)
-          commentVoteCount.positive++;
-        else if (sentiment.compound <= -BASIC_SENTIMENT_THRESHOLD)
-          commentVoteCount.negative++;
-        else commentVoteCount.neutral++;
-
-        avg.positive += sentiment.pos;
-        avg.neutral += sentiment.neu;
-        avg.negative += sentiment.neg;
-        avg.compound += sentiment.compound;
-      } else {
-        commentCount.excluded++;
-      }
-      commentCount.all++;
-    }
-    commentCount.processed = commentCount.all - commentCount.excluded;
-
-    Object.keys(avg).forEach((key) => {
-      if (avg[key] !== 0) {
-        avg[key] /= commentCount.processed;
-      }
-    });
-
-    const vote =
-      commentCount.processed === 0
-        ? 'unknown'
-        : stateCompound(avg.compound, commentCount.processed);
-
-    res.status(200).json({
-      vote,
-      commentCount,
-      commentVoteCount,
-      avg,
-      date: new Date().toISOString(),
-    });
+    res.status(200).json(result);
   } catch (error: any) {
     next({ message: error.message, status: error.status });
   }
